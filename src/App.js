47d3d03d3b7b3fd36f27484d1ee3a936a018235b
MP <mm.potapov@mail.ru>
2022-12-24 02:55:59 +0300

    feat: foundation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page__container">
+      <header className="header">
+        <a className="header__logo" href="#"></a>
       </header>
-    </div>
-  );
+      <main className="content">
+        <section className="profile" aria-label="Профиль">
+          <div className="profile__account">
+            <img className="profile__avatar" src="#" alt="Фото профиля" />
+              <button className="profile__avatar-edit" aria-label="Изменить фото"></button>
+              <div className="profile__info">
+                <h1 className="profile__name"></h1>
+                <button type="button" className="profile__edit" aria-label="Редактировать"></button>
+                <p className="profile__profession"></p>
+              </div>
+          </div>
+          <button type="button" className="profile__button-add" aria-label="Добавить"></button>
+        </section>
+
+        <section className="elements" aria-label="Галерея мест">
+          <ul className="elements__list">
+          </ul>
+        </section>
+
+      </main>
+      <footer className="footer">
+        <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
+      </footer>
+
+      <template id="card">
+        <li className="elements__card">
+          <article className="element">
+            <img className="element__photo" src="#" alt="" />
+              <div className="element__footer">
+                <h2 className="element__name"></h2>
+                <div className="element__like-container">
+                  <button type="button" className="element__like"></button>
+                  <p className="element__like-counter">0</p>
+                </div>
+              </div>
+              <button type="button" className="element__delete" aria-label="Удалить"></button>
+          </article>
+        </li>
+      </template>
+
+      <div className="popup popup_type_profile">
+        <div className="popup__container">
+          <button type="button" className="popup__close" aria-label="Закрыть"></button>
+          <form className="popup__form" name="profile-form" novalidate>
+            <h2 className="popup__header">Редактировать профиль</h2>
+            <input type="text" name="name" className="popup__input popup__input_value_name" id="name-input" required
+              placeholder="Имя" value="Имя" minlength="2" maxlength="40" />
+              <span className="popup__error name-input-error"></span>
+              <input type="text" name="profession" className="popup__input popup__input_value_profession" placeholder="О себе"
+                id="profession-input" value="О себе" required minlength="2" maxlength="200" />
+                <span className="popup__error profession-input-error"></span>
+                <button type="submit" className="popup__save">Сохранить</button>
+              </form>
+            </div>
+        </div>
+
+        <div className="popup popup_type_card">
+          <div className="popup__container">
+            <button type="button" className="popup__close" aria-label="Закрыть"></button>
+            <form className="popup__form" name="profile-form" novalidate>
+              <h2 className="popup__header">Новое место</h2>
+              <input type="text" name="place" className="popup__input popup__input_value_place" required placeholder="Название"
+                id="place-input" minlength="2" maxlength="20" />
+                <span className="popup__error place-input-error"></span>
+                <input type="url" name="link" className="popup__input popup__input_value_link" required id="link-input"
+                  placeholder="Ссылка на изображение" />
+                  <span className="popup__error link-input-error"></span>
+                  <button type="submit" className="popup__save popup__save_disabled" disabled>Создать</button>
+                </form>
+              </div>
+          </div>
+
+          <div className="popup popup_type_photo">
+            <div className="popup__container">
+              <button type="button" className="popup__close" aria-label="Закрыть"></button>
+              <figure className="popup__figure">
+                <img className="popup__photo" src="#" alt="" />
+                  <figcaption className="popup__caption"></figcaption>
+              </figure>
+            </div>
+          </div>
+
+          <div className="popup popup_type_delete">
+            <div className="popup__container">
+              <button type="button" className="popup__close" aria-label="Закрыть"></button>
+              <form className="popup__form" name="delete-form">
+                <h2 className="popup__header">Вы уверены?</h2>
+                <button type="submit" className="popup__save">Да</button>
+              </form>
+            </div>
+          </div>
+
+          <div className="popup popup_type_avatar">
+            <div className="popup__container">
+              <button type="button" className="popup__close" aria-label="Закрыть"></button>
+              <form className="popup__form" name="avatar-form" novalidate>
+                <h2 className="popup__header">Обновить аватар</h2>
+                <input type="url" name="avatar" className="popup__input popup__input_value_avatar" required placeholder="Ссылка"
+                  id="avatar-input" />
+                  <span className="popup__error avatar-input-error"></span>
+                  <button type="submit" className="popup__save">Сохранить</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        );
 }
 
-export default App;
+        export default App;
